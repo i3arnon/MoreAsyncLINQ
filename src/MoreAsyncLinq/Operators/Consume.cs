679eec49951a39a8295b67c19998a72a8e5ecb58
Bar Arnon <i3arnon@gmail.com>
2021-03-23 03:22:26 +0200

    Operators | Consume

---
+++ src/MoreAsyncLinq/Operators/Consume.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MoreAsyncLinq
+{
+    static partial class MoreAsyncEnumerable
+    {
+        public static ValueTask ConsumeAsync<TSource>(
+            this IAsyncEnumerable<TSource> source,
+            CancellationToken cancellationToken = default)
+        {
+            if (source is null) throw new ArgumentNullException(nameof(source));
+
+            return Core(source, cancellationToken);
+
+            static async ValueTask Core(
+                IAsyncEnumerable<TSource> source,
+                CancellationToken cancellationToken)
+            {
+                await foreach (var _ in source.WithCancellation(cancellationToken).ConfigureAwait(false))
+                {
+                }
+            }
+        }
+    }
+}
