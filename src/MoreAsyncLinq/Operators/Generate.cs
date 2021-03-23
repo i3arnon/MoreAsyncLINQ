d8c0df6935a41c310420b5c01ea829fc9ad6f467
Bar Arnon <i3arnon@gmail.com>
2021-03-23 19:13:52 +0200

    Operators | Generate

---
+++ src/MoreAsyncLinq/Operators/Generate.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MoreAsyncLinq
+{
+    static partial class MoreAsyncEnumerable
+    {
+        public static IAsyncEnumerable<TResult> GenerateAwait<TResult>(
+            TResult initial,
+            Func<TResult, ValueTask<TResult>> generator)
+        {
+            if (generator is null) throw new ArgumentNullException(nameof(generator));
+
+            return Core(initial, generator);
+
+            static async IAsyncEnumerable<TResult> Core(
+                TResult initial,
+                Func<TResult, ValueTask<TResult>> generator,
+                [EnumeratorCancellation] CancellationToken cancellationToken = default)
+            {
+                var current = initial;
+                while (true)
+                {
+                    yield return current;
+                    current = await generator(current).ConfigureAwait(false);
+                    cancellationToken.ThrowIfCancellationRequested();
+                }
+            }
+        }
+    }
+}
