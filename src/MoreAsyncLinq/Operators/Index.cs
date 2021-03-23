2cd7ba6b8ce329799e0bd4c203a2c6fa5bfa1de4
Bar Arnon <i3arnon@gmail.com>
2021-03-23 19:05:31 +0200

    Operators | Index

---
+++ src/MoreAsyncLinq/Operators/Index.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MoreAsyncLinq
+{
+    static partial class MoreAsyncEnumerable
+    {
+        public static IAsyncEnumerable<(int Index, TSource Element)> Index<TSource>(this IAsyncEnumerable<TSource> source)
+        {
+            if (source is null) throw new ArgumentNullException(nameof(source));
+            
+            return source.Index(startIndex: 0);
+        }
+
+        public static IAsyncEnumerable<(int Index, TSource Element)> Index<TSource>(this IAsyncEnumerable<TSource> source, int startIndex)
+        {
+            if (source is null) throw new ArgumentNullException(nameof(source));
+
+            return source.Select((element, index) => (startIndex + index, element));
+        }
+    }
+}
