a89972c51c3771e976051424bced3ca87289371e
Bar Arnon <i3arnon@gmail.com>
2021-03-23 19:33:00 +0200

    Operators | GenerateByIndex

---
+++ src/MoreAsyncLinq/Operators/GenerateByIndex.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+using static System.Linq.Enumerable;
+
+namespace MoreAsyncLinq
+{
+    static partial class MoreAsyncEnumerable
+    {
+        public static IAsyncEnumerable<TResult> GenerateByIndexAwait<TResult>(Func<int, ValueTask<TResult>> generator)
+        {
+            if (generator is null) throw new ArgumentNullException(nameof(generator));
+
+            return Range(start: 0, int.MaxValue).ToAsyncEnumerable().SelectAwait(generator);
+        }
+    }
+}
