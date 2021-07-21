f6ad235c8514383b371a638a404106cfb7cd7fca
Bar Arnon <i3arnon@gmail.com>
2021-07-21 14:00:26 +0300

    Split OrderBy and ThenBy

--- src/MoreAsyncLINQ/Operators/ThenBy.cs
+++ src/MoreAsyncLINQ/Operators/ThenBy.cs
@@ -8,19 +8,19 @@ namespace MoreAsyncLINQ
 {
     static partial class MoreAsyncEnumerable
     {
-        public static IOrderedAsyncEnumerable<TSource> OrderBy<TSource, TKey>(
-            this IAsyncEnumerable<TSource> source,
+        public static IOrderedAsyncEnumerable<TSource> ThenBy<TSource, TKey>(
+            this IOrderedAsyncEnumerable<TSource> source,
             Func<TSource, TKey> keySelector,
             OrderByDirection direction)
         {
             if (source is null) throw new ArgumentNullException(nameof(source));
             if (keySelector is null) throw new ArgumentNullException(nameof(keySelector));
-            
-            return source.OrderBy(keySelector,comparer: null, direction);
+
+            return source.ThenBy(keySelector, comparer: null, direction);
         }
 
-        public static IOrderedAsyncEnumerable<TSource> OrderBy<TSource, TKey>(
-            this IAsyncEnumerable<TSource> source,
+        public static IOrderedAsyncEnumerable<TSource> ThenBy<TSource, TKey>(
+            this IOrderedAsyncEnumerable<TSource> source,
             Func<TSource, TKey> keySelector,
             IComparer<TKey>? comparer,
             OrderByDirection direction)
@@ -30,23 +30,23 @@ namespace MoreAsyncLINQ
 
             comparer ??= Comparer<TKey>.Default;
             return direction == Ascending
-                ? source.OrderBy(keySelector, comparer)
-                : source.OrderByDescending(keySelector, comparer);
+                ? source.ThenBy(keySelector, comparer)
+                : source.ThenByDescending(keySelector, comparer);
         }
 
-        public static IOrderedAsyncEnumerable<TSource> OrderByAwait<TSource, TKey>(
-            this IAsyncEnumerable<TSource> source,
+        public static IOrderedAsyncEnumerable<TSource> ThenByAwait<TSource, TKey>(
+            this IOrderedAsyncEnumerable<TSource> source,
             Func<TSource, ValueTask<TKey>> keySelector,
             OrderByDirection direction)
         {
             if (source is null) throw new ArgumentNullException(nameof(source));
             if (keySelector is null) throw new ArgumentNullException(nameof(keySelector));
 
-            return source.OrderByAwait(keySelector, comparer: null, direction);
+            return source.ThenByAwait(keySelector, comparer: null, direction);
         }
 
-        public static IOrderedAsyncEnumerable<TSource> OrderByAwait<TSource, TKey>(
-            this IAsyncEnumerable<TSource> source,
+        public static IOrderedAsyncEnumerable<TSource> ThenByAwait<TSource, TKey>(
+            this IOrderedAsyncEnumerable<TSource> source,
             Func<TSource, ValueTask<TKey>> keySelector,
             IComparer<TKey>? comparer,
             OrderByDirection direction)
@@ -56,8 +56,8 @@ namespace MoreAsyncLINQ
 
             comparer ??= Comparer<TKey>.Default;
             return direction == Ascending
-                ? source.OrderByAwait(keySelector, comparer)
-                : source.OrderByDescendingAwait(keySelector, comparer);
+                ? source.ThenByAwait(keySelector, comparer)
+                : source.ThenByDescendingAwait(keySelector, comparer);
         }
     }
 }
