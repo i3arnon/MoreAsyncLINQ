67ce0578de9ae6e9ca4414d4a3d3ea4224dc40c1
Bar Arnon <i3arnon@gmail.com>
2026-01-14 21:24:47 +0200

    Modernize Window & WindowLeft (#24)

--- src/MoreAsyncLINQ/Operators/WindowRight.cs
+++ src/MoreAsyncLINQ/Operators/WindowRight.cs
@@ -35,12 +35,12 @@ static partial class MoreAsyncEnumerable
 
         return source.IsKnownEmpty()
             ? AsyncEnumerable.Empty<IList<TSource>>()
-            : Core(source, size);
+            : Core(source, size, default);
 
         static async IAsyncEnumerable<IList<TSource>> Core(
             IAsyncEnumerable<TSource> source,
             int size,
-            [EnumeratorCancellation] CancellationToken cancellationToken = default)
+            [EnumeratorCancellation] CancellationToken cancellationToken )
         {
             var window = new List<TSource>(size);
             await foreach (var element in source.WithCancellation(cancellationToken))
